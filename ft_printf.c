5412999dcc35e8efb2349148959387d6654b2f8f
Yaroslav YATSENKO <yyatsenk@e1r8p8.unit.ua>
2018-04-16 15:57:49 +0300

    done

---
+++ ft_printf.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyatsenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 17:23:50 by yyatsenk          #+#    #+#             */
+/*   Updated: 2017/12/15 17:23:51 by yyatsenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./ft_printf.h"
+
+static int		proc_finder(char **format)
+{
+	int			count;
+
+	count = 0;
+	while (**format != '%' && **format)
+	{
+		write(1, &**format, 1);
+		*format = *format + 1;
+		count++;
+	}
+	return (count);
+}
+
+int				ft_printf(char *format, ...)
+{
+	t_perech	data;
+	t_typer		type;
+	int			retur;
+	va_list		ap;
+
+	retur = 0;
+	va_start(ap, format);
+	while (*format)
+	{
+		data = no;
+		type = none;
+		retur += proc_finder(&format);
+		if (*format == '\0')
+			return (retur);
+		retur += way_definder(format_cat(&format), &ap, &type, &data);
+	}
+	va_end(ap);
+	return (retur);
+}
